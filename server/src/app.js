2611c4d9ec7cc6e21ebd3438f22b20f81159d9bc
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-09-11 01:50:22 +0530

    Update portfolio frontend and backend

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://portfolio-git-main-codedby-pratyushs-projects.vercel.app/",
+    origin: "https://portfolio-git-main-codedby-pratyushs-projects.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
